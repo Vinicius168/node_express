d46704370c85007a1226c02c316f8d525f96044c
Vinicius Alves <viniciusgames168@gmail.com>
2023-11-06 15:43:27 -0300

    recebendo informaçoes do formulário no corpo da requisição

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const express = require ("express")
+const express = require("express")
 const path = require("path")
 
 const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.use(express.urlencoded({
+    extended:true
+}))
+
+app.use(express.json())
+
+app.post('/cadastrar/salvar', (requisicao, resposta) => {
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+    
+    console.log(`O email do usuário é: ${email}`)
+})
+
+app.get('/cadastrar', (requisicao, resposta) => {
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
+
 // https://localhost:3000/usuarios/20
 app.get('/usuarios/:id', (requisicao, resposta) => {
     const id = requisicao.params.id
 
-    console.log(id)
+    console.log(`Acessando dados do usuário ${id}`)
 
     resposta.sendFile(`${caminhoBase}/usuarios.html`)
 })
@@ -17,10 +35,6 @@ app.get('/usuarios/:id', (requisicao, resposta) => {
   // https://localhost:3000
 app.get ('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
-})
-
-app.get ('/', (requisicao, resposta) => {
-    //resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
